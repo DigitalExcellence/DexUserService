668b5f832ddfa8b39eab8e9bb871d53d08af5282
Mees van Straten <41986758+MeesvanStraten@users.noreply.github.com>
2021-10-13 10:24:58 +0200

    added user seed

---
+++ Data/Context.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+        : base(options)
+        {
+        }
+        public DbSet<User> User { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasData(new User { FirstName = "John", lastName = "Smith" });
+            modelBuilder.Entity<User>().HasData(new User { FirstName = "Bob", lastName = "Bobbers" });
+            modelBuilder.Entity<User>().HasData(new User { FirstName = "Alice", lastName = "Garcia" });
+        }
+    }
+}
